4773c0cf34f740da81a17468294564d8181c7717
Giancarlo <gian-k@.live.com>
2020-12-13 12:49:09 -0300

    propiedades jsx

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,31 +1,21 @@
 import React from 'react'
 
-const mayorDeEdad = edad => edad > 18
-
-const persona = {"nombre" : "Alberto", "apellido": "Quiroga", "edad": 29}
+const curso = {
+    "title" : "React desde cero",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/big/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg",
+    "price": "50usd"
+}
 
 const Curso = () => (
     <>
         <article className="card">
             <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-                <img src="https://ux.ed.team/img/curso-portada.png" alt="card" />
+                <img src={curso.image} alt={curso.title} />
             </div>
             <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-                {
-                    mayorDeEdad(persona.edad) ? <h3>Soy mayor</h3> : <p>Soy menor</p>
-                }
-                <div className="s-mb-2 s-main-center">
-                    <div className="card__teacher s-cross-center">
-                        <div className="card__avatar s-mr-1">
-                            <div className="circle img-container">
-                                <img src="https://ux.ed.team/img/profesor.jpg" alt="teacher" />
-                            </div>
-                        </div>
-                    <span className="small">{ `${persona.nombre} ${persona.apellido}` }</span>
-                    </div>
-                </div>
+                <h3 className="center">{curso.title}</h3>
                 <div className="s-main-center">
-                    <a className="button--ghost-alert button--tiny" href="https://ed.team">$ 20USD</a>
+                    <a className="button--ghost-alert button--tiny" href="https://ed.team">{ `$ ${curso.price}` }</a>
                 </div>
             </div>
         </article>   
