2e35b232ac7a40ea6cd46cf3606659b7d33d8655
Giancarlo <gian-k@.live.com>
2020-12-13 15:33:29 -0300

    mapeo de cursos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import React from 'react'
 import "./styles/styles.scss"
 import Curso from './Curso'
 
+const cursos = [
+    {
+        "title": "React desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/courses/big/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg",
+        "price": 40,
+        "professor": "Beto Quiroga"
+    },
+    {
+        "title": "Drupal desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/courses/big/43973b1c-a8e2-4863-9252-2ae41377cab2.png",
+        "price": 30,
+        "professor": "Beto Quiroga"
+    },
+    {
+        "title": "Go desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/courses/big/3ec614b5-a9b7-475c-bf8f-8ba643a129eb.png",
+        "price": 50,
+        "professor": "Alexys Lozada"
+    },
+    {
+        "title": "HTML desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/courses/big/26557907-0555-427e-a40c-6ff207f98d72.png",
+        "price": 10,
+        "professor": "Alvaro Felipe"
+    },
+]
+
+
 const App = () => (
     <>
         <div className="main-banner img-container l-section" id="main-banner">
@@ -17,19 +45,15 @@ const App = () => (
             </div>
         </div>
         <div className="ed-grid m-grid-3">
-            <Curso
-                title= "React desde cero"
-                image= "https://edteam-media.s3.amazonaws.com/courses/big/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg"
-                price= "30"
-                professor= "Beto Quiroga"
-            />
-            <Curso
-                title= "HMTL desde cero"
-                image= "https://edteam-media.s3.amazonaws.com/courses/big/26557907-0555-427e-a40c-6ff207f98d72.png"
-                price= "28"
-                professor= "Álvaro Felipe"
-            />
-            <Curso price={20}/>
+            {
+                cursos.map( c =>
+                    <Curso
+                        title={c.title}
+                        image={c.image}
+                        price={c.price}
+                        professor={c.professor}
+                    />)
+            }
         </div>
     </>
 
