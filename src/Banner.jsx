4ed7012b4cc15adb83eb415a57bc6cf1e971b8be
Giancarlo <gian-k@.live.com>
2020-12-14 01:31:18 -0300

    creando componente de clase

---
+++ src/Banner.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Banner = () => (
+    <div className="main-banner img-container l-section" id="main-banner">
+    <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+            <img className="main-banner__img" alt="banner" src="https://i.pinimg.com/originals/c8/fa/45/c8fa45c7cf32e04c991b390908efcb2e.jpg" />
+            <div className="main-banner__data s-center">
+                <p className="t2 s-mb-0">Título del banner</p>
+                <p> Subtítulo del banner</p>
+                <a href="https://ed.team" className="button">Botón del banner</a>
+            </div>
+        </div>  
+    </div>
+</div>
+)
+
+export default Banner
---
+++ src/Formulario.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from "react"
+
+class Formulario extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="ed-grid">
+                <h1>Formulario</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Formulario
