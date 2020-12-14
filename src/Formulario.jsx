f54fe34f2f6ca9727282820b64b01b023cc82748
Giancarlo <gian-k@.live.com>
2020-12-14 01:37:29 -0300

    creado el formulario

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -10,6 +10,19 @@ class Formulario extends Component {
         return (
             <div className="ed-grid">
                 <h1>Formulario</h1>
+                <form>
+                    <div className="form__item">
+                        <label>Nombre Completo</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="form__item">
+                        <label>Correo Electrónico</label>
+                        <input type="email"/>
+                    </div>
+                    <div className="form__item">
+                        <input type="submit" className="button full" value="Enviar"/>
+                    </div>
+                </form>
             </div>
         )
     }
