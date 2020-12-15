b35b32cb08ea6ee1025743ca6a1d820e25a8b618
Giancarlo <gian-k@.live.com>
2020-12-15 20:18:39 -0300

    links dinámicos sin recarga de sitio (componente Link)

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from "react-router-dom"
 
 
-const Curso = ({title, image, price, professor}) => (
+const Curso = ({id, title, image, price, professor}) => (
     <>
         <article className="card">
             <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-                <img src={image} alt={title} />
+                <Link to={`/cursos/${id}`}>
+                    <img src={image} alt={title} />
+                </Link>
             </div>
             <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
                 <h3 className="center">{title}</h3>
