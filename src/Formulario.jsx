22149b00197b4c387a21cbd07843a663bff00182
Giancarlo <gian-k@.live.com>
2020-12-14 21:51:49 -0300

    limpiando el intervalo de fecha

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -7,12 +7,14 @@ class Formulario extends Component {
 
         this.state = {
             nombre: "",
-            correo: ""
+            correo: "",
+            fecha: new Date()
         }
 
 
         this.cambiarNombre = this.cambiarNombre.bind(this)
         this.cambiarCorreo = this.cambiarCorreo.bind(this)
+        this.cambiarFecha = this.cambiarFecha.bind(this)
     }
 
 
@@ -28,11 +30,18 @@ class Formulario extends Component {
         })
     }
 
+    cambiarFecha(){
+        this.setState({
+            fecha: new Date()
+        })
+    }
+
 
     render() {
          return (
             <div className="ed-grid">
                 <h1>Formulario {this.props.name}</h1>
+                <h4>Fecha actual: {Math.ceil(this.state.fecha/1000)}</h4>
                 <form id="elemento">
                     <div className="ed-grid m-grid-2">
                         <div className="form__item">
@@ -57,8 +66,20 @@ class Formulario extends Component {
 
     componentDidMount(){
         let elemento = document.getElementById("elemento")
-        console.log(elemento)
+        this.intervaloFecha = setInterval(() =>
+        { this.cambiarFecha()
+            console.log(new Date())}
+         , 1000)
+    }
 
+    componentDidUpdate(prevProps, prevState){
+        //console.log(prevProps)
+        //console.log(prevState)
+        //console.log("-------------")
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.intervaloFecha)
     }
 
 }
